f20e2d8aee5e2d9a2fe6d45124dbbdb2bea9b796
yashdeep singh <gosalyashdeep51@gmail.com>
2026-07-22 11:08:53 +0530

    javascript basics revision

---
+++ day1/index.js
@@ -0,0 +1,43 @@
+//console.log("hello world");
+
+//variables
+
+//var name="yashdeep";
+//console.log(name);
+//console.log(typeof(name)); 
+
+//let variable
+
+// let age=19;
+// age=20;
+// console.log(age);
+
+//const variable
+
+// const c="india";
+// console.log(c);
+
+// datatypes
+
+// let a=2;
+// let b="hello";
+// let c=true;
+
+// let a;
+// let b=null;
+// console.log(typeof(a));
+// console.log(typeof(b));
+
+// let a={
+//     name:"hello"
+// };
+// console.log(a);
+
+let a=5;
+let b=6;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
